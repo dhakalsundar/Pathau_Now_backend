b60b8b2c447e8213059f7ae328a7167002218e77
dhakalsundar <dhakalsundar440@gmail.com>
2026-01-20 12:45:40 +0545

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import { connectDatabase } from './database/mongodb';
-import { PORT } from './config';
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { connectDatabase } from "./database/mongodb";
+import { PORT } from "./config";
 import authRoutes from "./routes/auth.route";
 
 const app: Application = express();
@@ -9,31 +10,25 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req: Request, res: Response, next) => {
-    const origin = process.env.FRONTEND_URL || '*';
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204);
-    }
-    next();
-});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.use('/api/auth', authRoutes);
-app.get('/', (req: Request, res: Response) => {
-    return res.status(200).json({ success: "true", message: "Welcome to the API" });
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({ success: true, message: "Welcome to the API" });
 });
 
 async function startServer() {
-    await connectDatabase();
+  await connectDatabase();
 
-    app.listen(
-        PORT,
-        () => {
-            console.log(`Server: http://localhost:${PORT}`);
-        }
-    );
+  app.listen(PORT, () => {
+    console.log(`Server: http://localhost:${PORT}`);
+  });
 }
 
 startServer();
